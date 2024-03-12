71ef582c3b59763a611acb7570de2803c0380a0b
Garv2003 <aggarwalgarv0505@gmail.com>
2024-03-13 01:39:05 +0530

    changes

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,126 +1,132 @@
-import { useState, useRef } from "react";
-import "./Login.css";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { MdVisibilityOff, MdVisibility } from "react-icons/md";
-import { RotatingLines } from "react-loader-spinner";
-import { Link } from "@tanstack/react-router";
+// import { useState, useRef } from "react";
+// import "./Login.css";
+// import { useFormik } from "formik";
+// import * as Yup from "yup";
+// import { MdVisibilityOff, MdVisibility } from "react-icons/md";
+// import { RotatingLines } from "react-loader-spinner";
+// import { Link } from "@tanstack/react-router";
 
-function Login() {
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-    },
-    validationSchema: Yup.object({
-      username: Yup.string()
-        .required("Required")
-        .email("Invalid email address"),
-      password: Yup.string()
-        .required("Required")
-        .min(8, "Must be 8 characters or more")
-        .max(20, "Must be 20 characters or less"),
-    }),
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+// function Login() {
+//   const formik = useFormik({
+//     initialValues: {
+//       username: "",
+//       password: "",
+//     },
+//     validationSchema: Yup.object({
+//       username: Yup.string()
+//         .required("Required")
+//         .email("Invalid email address"),
+//       password: Yup.string()
+//         .required("Required")
+//         .min(8, "Must be 8 characters or more")
+//         .max(20, "Must be 20 characters or less"),
+//     }),
+//     onSubmit: (values) => {
+//       console.log(values);
+//     },
+//   });
 
-  const [showPassword, setShowPassword] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const passwordhidden = useRef(null);
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   const passwordhidden = useRef(null);
 
-  const changeVisibility = () => {
-    setShowPassword(!showPassword);
-    if (showPassword) {
-      passwordhidden.current.type = "password";
-    } else {
-      passwordhidden.current.type = "text";
-    }
-  };
+//   const changeVisibility = () => {
+//     setShowPassword(!showPassword);
+//     if (showPassword) {
+//       passwordhidden.current.type = "password";
+//     } else {
+//       passwordhidden.current.type = "text";
+//     }
+//   };
 
-  return (
-    <div className="login">
-      <div className="containerlogin">
-        <div className="box1">
-          <div className="headinglogin">Login</div>
-          <form className="login-form" onSubmit={formik.handleSubmit}>
-            <div className="field">
-              <input
-                id="username"
-                type="text"
-                className="login-input"
-                placeholder="Phone number, username, or email"
-                value={formik.values.username}
-                onChange={formik.handleChange}
-              />
-              {formik.touched.username && formik.errors.username ? (
-                <div className="error">{formik.errors.username}</div>
-              ) : null}
-            </div>
-            <div className="field">
-              <input
-                id="password"
-                value={formik.values.password}
-                className="login-input"
-                onChange={formik.handleChange}
-                type="password"
-                ref={passwordhidden}
-                placeholder="Password"
-                autoComplete="on"
-              />
-              {formik.touched.password && formik.errors.password ? (
-                <div className="error">{formik.errors.password}</div>
-              ) : null}
-              <div className="eye" onClick={() => changeVisibility()}>
-                {showPassword ? (
-                  <MdVisibilityOff
-                    style={{
-                      color: "#d7d7d7",
-                      fontSize: "20px",
-                    }}
-                  />
-                ) : (
-                  <MdVisibility
-                    style={{
-                      color: "grey",
-                      fontSize: "20px",
-                    }}
-                  />
-                )}
-              </div>
-            </div>
-            <button
-              className="login-button"
-              type="submit"
-              title="Login"
-              disabled={loading}
-            >
-              {loading ? (
-                <RotatingLines strokeColor="#fff" height={15} width={15} />
-              ) : (
-                "Log In"
-              )}
-            </button>
-            <div className="other">
-              {/* <Link className="forgot-password" to="">
-                Forgot password?
-              </Link> */}
-            </div>
-          </form>
-        </div>
-        <div className="box1">
-          <p>
-            Don&apos;t have an account?{" "}
-            <Link className="signup" to="/signup">
-              Sign Up
-            </Link>
-          </p>
-        </div>
-        <div className="footer">Developed and Designed by Garv Aggarwal</div>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="login">
+//       <div className="containerlogin">
+//         <div className="box1">
+//           <div className="headinglogin">Login</div>
+//           <form className="login-form" onSubmit={formik.handleSubmit}>
+//             <div className="field">
+//               <input
+//                 id="username"
+//                 type="text"
+//                 className="login-input"
+//                 placeholder="Phone number, username, or email"
+//                 value={formik.values.username}
+//                 onChange={formik.handleChange}
+//               />
+//               {formik.touched.username && formik.errors.username ? (
+//                 <div className="error">{formik.errors.username}</div>
+//               ) : null}
+//             </div>
+//             <div className="field">
+//               <input
+//                 id="password"
+//                 value={formik.values.password}
+//                 className="login-input"
+//                 onChange={formik.handleChange}
+//                 type="password"
+//                 ref={passwordhidden}
+//                 placeholder="Password"
+//                 autoComplete="on"
+//               />
+//               {formik.touched.password && formik.errors.password ? (
+//                 <div className="error">{formik.errors.password}</div>
+//               ) : null}
+//               <div className="eye" onClick={() => changeVisibility()}>
+//                 {showPassword ? (
+//                   <MdVisibilityOff
+//                     style={{
+//                       color: "#d7d7d7",
+//                       fontSize: "20px",
+//                     }}
+//                   />
+//                 ) : (
+//                   <MdVisibility
+//                     style={{
+//                       color: "grey",
+//                       fontSize: "20px",
+//                     }}
+//                   />
+//                 )}
+//               </div>
+//             </div>
+//             <button
+//               className="login-button"
+//               type="submit"
+//               title="Login"
+//               disabled={loading}
+//             >
+//               {loading ? (
+//                 <RotatingLines strokeColor="#fff" height={15} width={15} />
+//               ) : (
+//                 "Log In"
+//               )}
+//             </button>
+//             <div className="other">
+//               {/* <Link className="forgot-password" to="">
+//                 Forgot password?
+//               </Link> */}
+//             </div>
+//           </form>
+//         </div>
+//         <div className="box1">
+//           <p>
+//             Don&apos;t have an account?{" "}
+//             <Link className="signup" to="/signup">
+//               Sign Up
+//             </Link>
+//           </p>
+//         </div>
+//         <div className="footer">Developed and Designed by Garv Aggarwal</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
+const Login = () => {
+  return <div>Login</div>;
+};
 
 export default Login;
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,172 +1,178 @@
-import { MdVisibilityOff, MdVisibility } from "react-icons/md";
-import { RotatingLines } from "react-loader-spinner";
-import { useRef, useState } from "react";
-import "./Login/Login.css";
-import { useFormik } from "formik";
-import { Link } from "@tanstack/react-router";
-function SignUp() {
-  const [showPassword, setShowPassword] = useState(false);
-  const [showPassword2, setShowPassword2] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const passwordhidden = useRef(null);
-  const confirmpasswordhidden = useRef(null);
+// import { MdVisibilityOff, MdVisibility } from "react-icons/md";
+// import { RotatingLines } from "react-loader-spinner";
+// import { useRef, useState } from "react";
+// import "./Login/Login.css";
+// import { useFormik } from "formik";
+// import { Link } from "@tanstack/react-router";
+// function SignUp() {
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [showPassword2, setShowPassword2] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   const passwordhidden = useRef<HTMLInputElement>(null);
+//   const confirmpasswordhidden = useRef<HTMLInputElement>(null);
 
-  const changeVisibility = () => {
-    setShowPassword(!showPassword);
-    if (showPassword) {
-      passwordhidden.current.type = "password";
-    } else {
-      passwordhidden.current.type = "text";
-    }
-  };
+//   const changeVisibility = () => {
+//     setShowPassword(!showPassword);
+//     if (showPassword) {
+//       passwordhidden.current.type = "password";
+//     } else {
+//       passwordhidden.current.type = "text";
+//     }
+//   };
 
-  const changeVisibility2 = () => {
-    setShowPassword2(!showPassword2);
-    if (showPassword2) {
-      confirmpasswordhidden.current.type = "password";
-    } else {
-      confirmpasswordhidden.current.type = "text";
-    }
-  };
+//   const changeVisibility2 = () => {
+//     setShowPassword2(!showPassword2);
+//     if (showPassword2) {
+//       confirmpasswordhidden.current.type = "password";
+//     } else {
+//       confirmpasswordhidden.current.type = "text";
+//     }
+//   };
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      username: "",
-      email: "",
-      password: "",
-      confirmpassword: "",
-    },
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+//   const formik = useFormik({
+//     initialValues: {
+//       name: "",
+//       username: "",
+//       email: "",
+//       password: "",
+//       confirmpassword: "",
+//     },
+//     onSubmit: (values) => {
+//       console.log(values);
+//     },
+//   });
 
-  return (
-    <div className="login">
-      <div className="containerlogin">
-        <div className="box1">
-          <div className="headinglogin">Sign Up</div>
-          <form className="login-form" onSubmit={formik.handleSubmit}>
-            <div className="field">
-              <input
-                id="fullname"
-                type="name"
-                className="login-input"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                placeholder="Full Name"
-              />
-            </div>
-            <div className="field">
-              <input
-                id="username"
-                type="name"
-                className="login-input"
-                value={formik.values.username}
-                onChange={formik.handleChange}
-                placeholder="Username"
-              />
-            </div>
-            <div className="field">
-              <input
-                id="email"
-                type="name"
-                className="login-input"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                placeholder="Email"
-              />
-            </div>
-            <div className="field">
-              <input
-                id="password"
-                type="password"
-                ref={passwordhidden}
-                placeholder="Password"
-                className="login-input"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                autoComplete="off"
-              />
-              <div
-                className="eye"
-                onClick={() => {
-                  changeVisibility();
-                }}
-              >
-                {showPassword ? (
-                  <MdVisibilityOff
-                    style={{
-                      color: "#d7d7d7",
-                      fontSize: "20px",
-                    }}
-                  />
-                ) : (
-                  <MdVisibility
-                    style={{
-                      color: "grey",
-                      fontSize: "20px",
-                    }}
-                  />
-                )}
-              </div>
-            </div>
-            <div className="field">
-              <input
-                id="confirm password"
-                type="password"
-                placeholder="Confirm Password"
-                ref={confirmpasswordhidden}
-                className="login-input"
-                value={formik.values.confirmpassword}
-                onChange={formik.handleChange}
-                autoComplete="off"
-              />
-              <div
-                className="eye"
-                onClick={() => {
-                  changeVisibility2();
-                }}
-              >
-                {showPassword2 ? (
-                  <MdVisibilityOff
-                    style={{
-                      color: "#d7d7d7",
-                      fontSize: "20px",
-                    }}
-                  />
-                ) : (
-                  <MdVisibility
-                    style={{
-                      color: "grey",
-                      fontSize: "20px",
-                    }}
-                  />
-                )}
-              </div>
-            </div>
-            <button className="login-button" title="login" disabled={loading}>
-              {loading ? (
-                <RotatingLines strokeColor="#fff" height={15} width={15} />
-              ) : (
-                "Sign Up"
-              )}
-            </button>
-          </form>
-          <div className="hother">Sign up to see weather updates, and more</div>
-        </div>
-        <div className="box1">
-          <p>
-            Have an account?{" "}
-            <Link className="signup" to="/login">
-              Log in
-            </Link>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="login">
+//       <div className="containerlogin">
+//         <div className="box1">
+//           <div className="headinglogin">Sign Up</div>
+//           <form className="login-form" onSubmit={formik.handleSubmit}>
+//             <div className="field">
+//               <input
+//                 id="fullname"
+//                 type="name"
+//                 className="login-input"
+//                 // value={formik.values.name}
+//                 // onChange={formik.handleChange}
+//                 placeholder="Full Name"
+//               />
+//             </div>
+//             <div className="field">
+//               <input
+//                 id="username"
+//                 type="name"
+//                 className="login-input"
+//                 value={formik.values.username}
+//                 onChange={formik.handleChange}
+//                 placeholder="Username"
+//               />
+//             </div>
+//             <div className="field">
+//               <input
+//                 id="email"
+//                 type="name"
+//                 className="login-input"
+//                 value={formik.values.email}
+//                 onChange={formik.handleChange}
+//                 placeholder="Email"
+//               />
+//             </div>
+//             <div className="field">
+//               <input
+//                 id="password"
+//                 type="password"
+//                 ref={passwordhidden}
+//                 placeholder="Password"
+//                 className="login-input"
+//                 value={formik.values.password}
+//                 onChange={formik.handleChange}
+//                 autoComplete="off"
+//               />
+//               <div
+//                 className="eye"
+//                 onClick={() => {
+//                   changeVisibility();
+//                 }}
+//               >
+//                 {showPassword ? (
+//                   <MdVisibilityOff
+//                     style={{
+//                       color: "#d7d7d7",
+//                       fontSize: "20px",
+//                     }}
+//                   />
+//                 ) : (
+//                   <MdVisibility
+//                     style={{
+//                       color: "grey",
+//                       fontSize: "20px",
+//                     }}
+//                   />
+//                 )}
+//               </div>
+//             </div>
+//             <div className="field">
+//               <input
+//                 id="confirm password"
+//                 type="password"
+//                 placeholder="Confirm Password"
+//                 ref={confirmpasswordhidden}
+//                 className="login-input"
+//                 value={formik.values.confirmpassword}
+//                 onChange={formik.handleChange}
+//                 autoComplete="off"
+//               />
+//               <div
+//                 className="eye"
+//                 onClick={() => {
+//                   changeVisibility2();
+//                 }}
+//               >
+//                 {showPassword2 ? (
+//                   <MdVisibilityOff
+//                     style={{
+//                       color: "#d7d7d7",
+//                       fontSize: "20px",
+//                     }}
+//                   />
+//                 ) : (
+//                   <MdVisibility
+//                     style={{
+//                       color: "grey",
+//                       fontSize: "20px",
+//                     }}
+//                   />
+//                 )}
+//               </div>
+//             </div>
+//             <button className="login-button" title="login" disabled={loading}>
+//               {loading ? (
+//                 <RotatingLines strokeColor="#fff" height={15} width={15} />
+//               ) : (
+//                 "Sign Up"
+//               )}
+//             </button>
+//           </form>
+//           <div className="hother">Sign up to see weather updates, and more</div>
+//         </div>
+//         <div className="box1">
+//           <p>
+//             Have an account?{" "}
+//             <Link className="signup" to="/login">
+//               Log in
+//             </Link>
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SignUp;
+
+const SignUp = () => {
+  return <div>Signup</div>;
+};
 
 export default SignUp;
